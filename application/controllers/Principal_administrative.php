d1d863260a627cff2d04d05393ef18c6aa4970af
acuellar5 <acuellar_5@hotmail.com>
2018-06-01 16:58:23 -0500

    vidta modulos

--- application/controllers/Principal_administrative.php
+++ application/controllers/Principal_administrative.php
@@ -1,14 +1,14 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Principal_worker extends CI_Controller{
+class Principal_administrative extends CI_Controller{ //controlador siempre en mayuscula 
 	
 	function __construct(){
 		parent:: __construct();
 	}
 	//carga la vista principal del worker
-	public function v_principal_worker(){
+	public function v_principal_administrative(){
 		$this->load->view('parts/header');
-		$this->load->view('principal_worker');
+		$this->load->view('principal_administrative');
 		$this->load->view('parts/footer');
 	}
 	
