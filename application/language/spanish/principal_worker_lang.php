d0e63a2a626eefea7aad63d28e17c3a20b4bf55d
bredybuitrago <bredybuitrago@gmail.com>
2018-06-06 10:17:12 -0500

    principal worker 1%

---
+++ application/language/spanish/principal_worker_lang.php
@@ -0,0 +1,13 @@
+<?php 
+// titulos
+$lang['historial_kpi'] = "Historial kpi's";
+$lang['calificar_kpi'] = "Calificar kpi's";
+$lang['certificado'] = 'Certificado';
+$lang['historico'] = 'Historico';
+$lang['calificar'] = 'Calificar';
+//titulos tabla historial kpi
+$lang['proyecto'] = 'Proyecto';
+$lang['califico'] = 'Calificó';
+$lang['fecha_evaluo'] = 'Fecha evaluación';
+$lang['valor_mes'] = 'Valor mes vencido';
+$lang['por_kpi'] = '% kpi';
